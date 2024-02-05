b5a76597ea8497412175c6c2cb6eef3399921bf0
Pietrell <piotrplotast2006@gmail.com>
2024-02-05 18:05:37 +0100

    added custom color tailwind properties

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,10 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        gridCell: "#191C26",
+        mainBg: "#12151d",
+      },
       fontFamily: {
         main: ["'Baloo 2'", "sans-serif"],
       },
