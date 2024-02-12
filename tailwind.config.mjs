7b0e7aaab81c30a498d130ac21462b77ad8d3b0a
Pietrell <piotrplotast2006@gmail.com>
2024-02-12 14:33:51 +0100

    got rid of header component and added gradient to hero component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -23,6 +23,21 @@ export default {
       gridTemplateRows: {
         layout: "6rem 3rem 1fr auto",
       },
+      animation: {
+        text: "text 5s ease infinite",
+      },
+      keyframes: {
+        text: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
     },
   },
   plugins: [require("@savvywombat/tailwindcss-grid-areas")],
