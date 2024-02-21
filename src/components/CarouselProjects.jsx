b2a4fd86a02662a9cf8ca104721c52ac74b8b5a0
Pietrell <piotrplotast2006@gmail.com>
2024-02-21 17:23:14 +0100

    updated projects section

--- src/components/CarouselProjects.jsx
+++ src/components/CarouselProjects.jsx
@@ -11,30 +11,30 @@ import { Github, MonitorSmartphone } from "lucide-react";
 const projects = [
   {
     image:
-      "https://www.frontendmentor.io/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdz209s6jk%2Fimage%2Fupload%2Fv1564914816%2FChallenges%2Ftjmihf4uv326gkkz7mbk.jpg&w=384&q=75",
-    title: "Project 1",
+      "https://res.cloudinary.com/dz209s6jk/image/upload/f_auto,q_auto,w_700/Challenges/xiydlw7ggmnfnldlflit.jpg",
+    title: "Clipboard landing page",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-    repository: "https://github.com/PiotrPlotast",
-    livePreview: "https://www.google.com",
+      "Clipboard landing page frontend mentor project using tailwindcss.",
+    repository: "https://github.com/PiotrPlotast/clipboard-landing-page",
+    livePreview: "https://piotrplotast.github.io/clipboard-landing-page/",
   },
   {
     image:
-      "https://www.frontendmentor.io/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdz209s6jk%2Fimage%2Fupload%2Fv1652196092%2FChallenges%2Fbnjpxmx9fudsmerfj6eo.jpg&w=384&q=75",
-    title: "Project 2",
+      "https://private-user-images.githubusercontent.com/89207478/296444055-30f4e74c-ace2-4b0d-87f0-70fb7a45fab4.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDg1MzIwNzgsIm5iZiI6MTcwODUzMTc3OCwicGF0aCI6Ii84OTIwNzQ3OC8yOTY0NDQwNTUtMzBmNGU3NGMtYWNlMi00YjBkLTg3ZjAtNzBmYjdhNDVmYWI0LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDAyMjElMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwMjIxVDE2MDkzOFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTQ2NjY4NTliNGQzYzViNTdmOGNhNWM0ZjViNmYzNDE0MmRiMWU1NzE0NDgyN2FiOTk1OTRmNTUyOGU1NWIyMWUmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.V1avLmGSWLUmgKIgTG9y3i369g8yTWEWUu50NA4S-ko",
+    title: "Interactive Eisenhower Matrix",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-    repository: "https://github.com/PiotrPlotast",
-    livePreview: "https://www.google.com",
+      "Interactive Eisenhower Matrix is a web app in which you can categorize, manage and save your tasks between sessions.",
+    repository: "https://github.com/PiotrPlotast/interactive-eisenhower-matrix",
+    livePreview: "https://piotrplotast.github.io/interactive-eisenhower-matrix/",
   },
   {
     image:
-      "https://www.frontendmentor.io/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdz209s6jk%2Fimage%2Fupload%2Fv1554894929%2FChallenges%2Fqjiprcu1e19yvujjrflv.jpg&w=384&q=75",
-    title: "Project 3",
+      "https://res.cloudinary.com/dz209s6jk/image/upload/f_auto,q_auto,w_700/Challenges/wwrbx1zs3x2wiferhyaw.jpg",
+    title: "(WIP) Typemaster pre launch landing page",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-    repository: "https://github.com/PiotrPlotast",
-    livePreview: "https://www.google.com",
+      "Typemaster pre launch landing page frontend mentor project.",
+    repository: "https://github.com/PiotrPlotast/typemaster-pre-launch-landing-page",
+    livePreview: "https://piotrplotast.github.io/typemaster-pre-launch-landing-page/",
   },
 ];
 
